3abcfb2b3379a30a3e02a2b220923cdd3584b8d6
Phil Heslop <phil.heslop@gmail.com>
2025-11-06 18:55:10 +0000

    adds 'naive' rate password

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+def rate_password(password: str) -> str:
+    length = len(password)
+    if length < 6:
+        return "Weak"
+    elif 6 <= length < 10:
+        return "Moderate"
+    else:
+        return "Strong"
+
+
+@app.post("/rate-password/")
+def rate_password_endpoint(password: str):
+    return {"password": password, "strength": rate_password(password)}
